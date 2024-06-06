0eb7652193b61d4d1d43d1d954492010b7c78eae
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-06-06 16:15:11 -0400

    initial commit

---
+++ scripts/1-basics.r
@@ -0,0 +1,22 @@
+## R Basics
+
+## how to run commands?
+## crtl + enter
+
+## how to get help in R?
+## just use ? command
+?data
+?mean
+
+## operations in R
+## very similar to any other lang
+## sum
+1 + 1
+## sub
+2 - 3
+## div
+2 / 2
+## mult
+2 * 2
+## mod - att 2x% not 1X%
+3 %% 2
